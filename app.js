4b9f88bf2e51ede8a625e66425b4a20bed9fec47
Nisar Ahmad <nisar.a@mblhightech.com>
2022-02-28 17:52:05 +0200

    working on adding data

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { uid } from "./uid.js";
+console.log(uid())
 const IDB = (function init() {
   let db = null;
   let objectStore = null;
@@ -19,12 +21,54 @@ const IDB = (function init() {
   //Upgraded db
   DbOpenReq.addEventListener('upgradeneeded', (e) => {
     db = e.target.result;
+    let oldVersion = e.oldVersion;
+    let newVersion = e.newVersion || db.version;
+    console.log('DB updated from version', oldVersion, 'to', newVersion);
+
     console.log('upgrade', db);
-    if (!db.objectStoreNames.contains("StudentDB")) {
-      objectStore = db.createObjectStore('StudentDb', {
+    if (!db.objectStoreNames.contains("StudentStore")) {
+      objectStore = db.createObjectStore('StudentStore', {
         keyPath: 'id'
       });
     }
   });
+  document.studentForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let rollno = document.getElementById('stdId').value.trim();
+    let name = document.getElementById('stdName').value.trim();
+    let marks = parseInt(document.getElementById('stdMarks').value);
+    let department = document.getElementById('stdDepartment').value.trim();
+    let regular = document.getElementById('stdStatus').checked;
+    let student = {
+      id: uid(),
+      rollno,
+      name,
+      marks,
+      department,
+      regular,
+    };
+
+    let tx = makeTX('StudentStore', 'readwrite');
+    tx.oncomplete = (e) => {
+      console.log(e);
+    };
+
+    tx.onerror = (err) => {
+      console.warn(err);
+    };
+
+    let store = tx.objectStore('StudentStore');
+    let request = store.add(student);
+    request.onsuccess = (e) => {
+      console.log('successfully added in object');
+    };
+    request.onsuccess = (e) => {
+      console.log('error in request to add');
+    }
+  });
+  function makeTX(storeName, mode) {
+    let tx = db.transaction(storeName, mode);
+    return tx;
+  }
 })();
 
